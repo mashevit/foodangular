e26b04dfda9f9cfa28fd00fca71e2eb97f5441ac
mashevit <mashevit@gmail.com>
2018-11-11 23:42:00 +0200

    fdfdsfds

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname, '/src'));
+app.use(express.static(__dirname + '/dist/food-angular'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/src/index.html'));
+res.sendFile(path.join(__dirname+'/dist/food-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-//app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
