cfdeafd2fe1314114e42d0c8fc8f68675c11e60a
mashevit <mashevit@gmail.com>
2018-11-11 23:11:50 +0200

    sfdsd

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+// //Install express server
+// const express = require('express');
+// const path = require('path');
 
-const app = express();
+// const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/food-angular'));
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/food-angular'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/food-angular/index.html'));
-});
+// res.sendFile(path.join(__dirname+'/dist/food-angular/index.html'));
+// });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.listen(server_port, server_ip_address, function () {
+
+    console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
+
+});
