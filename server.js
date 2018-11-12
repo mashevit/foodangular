0d85e006a303420c29fe904b7693bc9158cdb575
mashevit <mashevit@gmail.com>
2018-11-12 11:55:23 +0200

    fds

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var express = require('express')
 
 //app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
 app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
-app.use(express.bodyParser());
-app.use(express.methodOverride());
+//app.use(express.bodyParser());
+//app.use(express.methodOverride());
 app.use(express.static(__dirname + '/public'));
 app.use('/components', express.static(__dirname + '/components'));
 app.use('/js', express.static(__dirname + '/js'));
