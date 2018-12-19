29a74435f128d0f4578d92f1ac6f945ac83f0606
mashevit <mashevit@gmail.com>
2018-12-19 23:16:14 +0200

    sdds  dsds

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
-const express = require('express');
-const http = require('http')
-const path = require('path');
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/food-angular'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/food-angular/index.html'));
 });
-
-const port = process.env.PORT || 3000;
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log('running'));
