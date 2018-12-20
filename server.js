3bea7faea864955e0498e9e5658a41f5ded587f4
mashevit <mashevit@gmail.com>
2018-12-20 06:48:26 +0200

    ssdds

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
-
 const express = require('express');
 const http = require('http')
 const path = require('path');
 const app = express();
 
-// Serve only the static files form the dist directory
+var express = require('express')
+ , app = express()
+ , server = require('http').createServer(app)
+ 
+ app.use(express.static(path.join(__dirname, 'dist')));
+
+
+
 app.use(express.static(__dirname + '/dist/'));
 app.use(express.static(__dirname + '/dist/food-angular'));
-
-app.get('/*', function(req,res) {
-
 res.sendFile(path.join(__dirname+'/dist/index.html'));
 res.sendFile(path.join(__dirname+'/dist/food-angular/index.html'));
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+app.get('/*', function(req,res) {
+ 
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/food-angular/index.html'));
+
+
+
 });
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running')); 
+
+
+ 
